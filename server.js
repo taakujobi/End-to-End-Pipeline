7b96c12c27087e3d731d7eb9019297769ea9a91d
Tochukwu <taakujobi@gmail.com>
2025-05-19 16:49:03 -0600

    step 4 - setting up Docker and pushing image 3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-let userGoal = 'Learn Docker!';
+let userGoal = 'Learn and test Pipeline!';
 
 app.use(
   bodyParser.urlencoded({
